561e3d7eb934523235ca3dc02bba3069636c0d0a
Susana Suárez <susanasuarezmendoza26@gmail.com>
2024-11-30 11:43:48 +0000

    odio github y vscode

---
+++ lucas-kanade/handperson.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class HandPerson:
+    def __init__(self):
+        self.x = []
+        self.y = []
+        self.speeds = []
+    
+    def add(self, x, y):
+        self.x.append(x)
+        self.y.append(y)
+        if len(self.x) > 1:
+            dx = x - self.x[-2]
+            dy = y - self.y[-2]
+            distance = np.sqrt(dx**2 + dy**2)
+            self.speeds.append(distance)
+    
+    def calculate_distance(self, x, y, threshold=30):
+        if len(self.x) > 0 and len(self.y) > 0:
+            distance = np.sqrt((x - self.x[-1])**2 + (y - self.y[-1])**2)
+            if distance < threshold:
+                self.add(x, y)
+            else:
+                return None
+        else:
+            self.add(x, y)
